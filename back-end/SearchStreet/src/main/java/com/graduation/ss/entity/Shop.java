c7412122ace920754884218066f98d5678bde1b5
Nilianfei <1074523347@qq.com>
2018-12-20 15:57:31 +0800

    add shop entity

---
+++ back-end/SearchStreet/src/main/java/com/graduation/ss/entity/Shop.java
@@ -0,0 +1,163 @@
+package com.graduation.ss.entity;
+
+import java.util.Date;
+
+public class Shop {
+	private Long shopId;
+	private String shopName;
+	private String shopImg;
+	private String businessLicenseImg;
+	private String businessLicenseCode;
+	private String phone;
+	private String province;
+	private String city;
+	private String district;
+	private String fullAddress;
+	private String shopMoreInfo;
+	//1:是移动商铺，0：不是移动商铺
+	private Integer isMobile;
+	private Date openTime;
+	private Date closeTime;
+	private String profileImg;
+	private Float coordinateX;
+	private Float coordinateY;
+	//0：审核中，1：许可，2：不许可
+	private Integer enableStatus;
+	private String businessScope;
+	private Date createTime;
+	private Date lastEditTime;
+	private PersonInfo personInfo;
+	
+	public Long getShopId() {
+		return shopId;
+	}
+	public void setShopId(Long shopId) {
+		this.shopId = shopId;
+	}
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getShopImg() {
+		return shopImg;
+	}
+	public void setShopImg(String shopImg) {
+		this.shopImg = shopImg;
+	}
+	public String getBusinessLicenseImg() {
+		return businessLicenseImg;
+	}
+	public void setBusinessLicenseImg(String businessLicenseImg) {
+		this.businessLicenseImg = businessLicenseImg;
+	}
+	public String getBusinessLicenseCode() {
+		return businessLicenseCode;
+	}
+	public void setBusinessLicenseCode(String businessLicenseCode) {
+		this.businessLicenseCode = businessLicenseCode;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getFullAddress() {
+		return fullAddress;
+	}
+	public void setFullAddress(String fullAddress) {
+		this.fullAddress = fullAddress;
+	}
+	public String getShopMoreInfo() {
+		return shopMoreInfo;
+	}
+	public void setShopMoreInfo(String shopMoreInfo) {
+		this.shopMoreInfo = shopMoreInfo;
+	}
+	public Integer getIsMobile() {
+		return isMobile;
+	}
+	public void setIsMobile(Integer isMobile) {
+		this.isMobile = isMobile;
+	}
+	public Date getOpenTime() {
+		return openTime;
+	}
+	public void setOpenTime(Date openTime) {
+		this.openTime = openTime;
+	}
+	public Date getCloseTime() {
+		return closeTime;
+	}
+	public void setCloseTime(Date closeTime) {
+		this.closeTime = closeTime;
+	}
+	public String getProfileImg() {
+		return profileImg;
+	}
+	public void setProfileImg(String profileImg) {
+		this.profileImg = profileImg;
+	}
+	public Float getCoordinateX() {
+		return coordinateX;
+	}
+	public void setCoordinateX(Float coordinateX) {
+		this.coordinateX = coordinateX;
+	}
+	public Float getCoordinateY() {
+		return coordinateY;
+	}
+	public void setCoordinateY(Float coordinateY) {
+		this.coordinateY = coordinateY;
+	}
+	public Integer getEnableStatus() {
+		return enableStatus;
+	}
+	public void setEnableStatus(Integer enableStatus) {
+		this.enableStatus = enableStatus;
+	}
+	public String getBusinessScope() {
+		return businessScope;
+	}
+	public void setBusinessScope(String businessScope) {
+		this.businessScope = businessScope;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+	public PersonInfo getPersonInfo() {
+		return personInfo;
+	}
+	public void setPersonInfo(PersonInfo personInfo) {
+		this.personInfo = personInfo;
+	}
+}
---
+++ database/tb_shop.sql
@@ -0,0 +1,48 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : root
+Source Server Version : 50723
+Source Host           : localhost:3306
+Source Database       : search_street
+
+Target Server Type    : MYSQL
+Target Server Version : 50723
+File Encoding         : 65001
+
+Date: 2018-12-20 15:55:21
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tb_shop
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_shop`;
+CREATE TABLE `tb_shop` (
+  `shop_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `shop_name` varchar(64) NOT NULL,
+  `shop_img` varchar(1024) DEFAULT NULL,
+  `business_license_code` varchar(128) DEFAULT NULL,
+  `business_license_img` varchar(1024) DEFAULT NULL,
+  `phone` varchar(15) NOT NULL,
+  `province` varchar(64) NOT NULL,
+  `city` varchar(64) NOT NULL,
+  `district` varchar(64) NOT NULL,
+  `full_address` varchar(255) NOT NULL,
+  `shop_more_info` varchar(255) DEFAULT NULL,
+  `is_mobile` int(2) NOT NULL DEFAULT '1',
+  `open_time` datetime DEFAULT NULL,
+  `close_time` datetime DEFAULT NULL,
+  `profile_img` varchar(1024) NOT NULL,
+  `coordinate_x` float NOT NULL,
+  `coordinate_y` float NOT NULL,
+  `enable_status` int(2) NOT NULL DEFAULT '0',
+  `business_scope` varchar(128) NOT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `last_edit_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`shop_id`),
+  KEY `fk_shop_profile` (`user_id`),
+  CONSTRAINT `fk_shop_profile` FOREIGN KEY (`user_id`) REFERENCES `tb_person_info` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
