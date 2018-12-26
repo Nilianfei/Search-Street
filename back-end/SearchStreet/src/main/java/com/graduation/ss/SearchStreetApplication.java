b77cbc374fe694a7be48f28f404a6d636414ce31
Nilianfei <1074523347@qq.com>
2018-12-26 17:28:02 +0800

    Change to Springboot

---
+++ back-end/SearchStreet/src/main/java/com/graduation/ss/SearchStreetApplication.java
@@ -0,0 +1,14 @@
+package com.graduation.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchStreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchStreetApplication.class, args);
+	}
+
+}
+
