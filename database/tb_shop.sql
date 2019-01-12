f1a2e09729c476048ff75f050d4c08544f68a605
Nilianfei <1074523347@qq.com>
2019-01-12 22:12:04 +0800

    tb_shop 增加人均消费

--- database/tb_shop.sql
+++ database/tb_shop.sql
@@ -1,17 +1,17 @@
 /*
  Navicat MySQL Data Transfer
 
- Source Server         : root
+ Source Server         : localhost_3306
  Source Server Type    : MySQL
- Source Server Version : 50723
+ Source Server Version : 50724
  Source Host           : localhost:3306
  Source Schema         : search_street
 
  Target Server Type    : MySQL
- Target Server Version : 50723
+ Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 31/12/2018 17:56:08
+ Date: 12/01/2019 22:10:30
 */
 
 SET NAMES utf8mb4;
@@ -25,9 +25,9 @@ CREATE TABLE `tb_shop`  (
   `shop_id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `shop_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `shop_img` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `business_license_code` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `business_license_img` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `per_cost` int(10) NULL DEFAULT 0,
   `phone` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `province` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `city` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
@@ -45,6 +45,6 @@ CREATE TABLE `tb_shop`  (
   `create_time` datetime(0) NULL DEFAULT NULL,
   `last_edit_time` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`shop_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 51 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
