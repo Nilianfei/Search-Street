ae9d7c80f99ab4887e315d332a51c157de2310a8
LiangWeixing <chunvzuoqzdt@126.com>
2019-03-26 18:42:28 +0800

    shop-settings

--- front-end/Ss1.4/page/shop-settings/shop-settings.js
+++ front-end/Ss1.4/page/shop-settings/shop-settings.js
@@ -12,6 +12,10 @@ Page({
     shop: {},
     latitude: 23.139317,
     longitude: 113.352354,
+    tempProvince:'',
+    tempCity:'',
+    tempDistrict: '',
+    markers: [],
     isMobile: [{
       id: 0,
       name: '固定商铺',
@@ -25,22 +29,20 @@ Page({
   handleIsMobileChange({
     detail = {}
   }) {
-    var p="shop.isMobile";
+    var p = "shop.isMobile";
     this.setData({
       current: detail.value,
     });
-    
-    if(this.data.current=="固定商铺"){
+
+    if (this.data.current == "固定商铺") {
       this.setData({
-        [p]: 0  
+        [p]: 0
       })
-    }
-    else if(this.data.current=="移动商铺") {
+    } else if (this.data.current == "移动商铺") {
       this.setData({
         [p]: 1
       })
-    }
-    else {
+    } else {
       console.log("error!");
     }
     console.log(this.data.shop.isMobile);
@@ -48,15 +50,15 @@ Page({
   /**
    * bind:change事件集合
    */
-  inputShopName:function(e) {
-    var p="shop.shopName";
+  inputShopName: function(e) {
+    var p = "shop.shopName";
     this.setData({
-      [p]:e.detail.detail.value
+      [p]: e.detail.detail.value
     });
     console.log(this.data.shop.shopName);
   },
 
-  inputPhone:function(e) {
+  inputPhone: function(e) {
     var p = "shop.phone";
     this.setData({
       [p]: e.detail.detail.value
@@ -64,7 +66,7 @@ Page({
     console.log(this.data.shop.phone);
   },
 
-  inputBusinessScope:function(e) {
+  inputBusinessScope: function(e) {
     var p = "shop.businessScope";
     this.setData({
       [p]: e.detail.detail.value
@@ -72,7 +74,7 @@ Page({
     console.log(this.data.shop.businessScope);
   },
 
-  inputPerCost:function(e) {
+  inputPerCost: function(e) {
     var p = "shop.perCost";
     this.setData({
       [p]: e.detail.detail.value
@@ -80,39 +82,36 @@ Page({
     console.log(this.data.shop.perCost);
   },
 
-  inputBusinessLicenceCode:function(e) {
-    var p = "shop.businessLicenceCode";
+  inputBusinessLicenceCode: function(e) {
+    var p = "shop.businessLicenseCode";
     this.setData({
       [p]: e.detail.detail.value
     });
-    console.log(this.data.shop.businessLicenceCode);
+    console.log(this.data.shop.businessLicenseCode);
   },
 
-  inputprovince: function (e) {
-    var p = "shop.province";
+  inputprovince: function(e) {
     this.setData({
-      [p]: e.detail.detail.value
+      tempProvince: e.detail.detail.value
     });
-    console.log(this.data.shop.province);
+    console.log(this.data.tempProvince);
   },
 
-  inputcity: function (e) {
-    var p = "shop.city";
+  inputcity: function(e) {
     this.setData({
-      [p]: e.detail.detail.value
+      tempCity: e.detail.detail.value
     });
-    console.log(this.data.shop.city);
+    console.log(this.data.tempCity);
   },
 
-  inputdistrict: function (e) {
-    var p = "shop.district";
+  inputdistrict: function(e) {
     this.setData({
-      [p]: e.detail.detail.value
+      tempDistrict: e.detail.detail.value
     });
-    console.log(this.data.shop.district);
+    console.log(this.data.tempDistrict);
   },
 
-  inputFullAddress: function (e) {
+  inputFullAddress: function(e) {
     var p = "shop.fullAddress";
     this.setData({
       [p]: e.detail.detail.value
@@ -120,7 +119,7 @@ Page({
     console.log(this.data.shop.fullAddress);
   },
 
-  inputShopMoreInfo: function (e) {
+  inputShopMoreInfo: function(e) {
     var p = "shop.shopMoreInfo";
     this.setData({
       [p]: e.detail.detail.value
@@ -128,15 +127,17 @@ Page({
     console.log(this.data.shop.shopMoreInfo);
   },
 
-/**
- * 生命周期函数--监听页面加载
- */
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function(options) {
     var that = this;
     console.log("get" + options.shopId);
     that.setData({
       shopId: options.shopId
     })
+    this.mapCtx = wx.createMapContext('myMap');
+
     //获取商铺信息
     wx.request({
       url: app.globalData.serviceUrl + "/SearchStreet/shopadmin/getshopbyid?shopId=" + that.data.shopId,
@@ -146,26 +147,36 @@ Page({
         console.log(res);
         var shop = res.data.shop;
         shop.profileImg = that.data.imgUrl + shop.profileImg;
-        that.setData(
-          {
-            shop: res.data.shop,//设置页面中的数据
-            longitude: res.data.shop.longitude,
+        that.setData({
+          shop: res.data.shop, //设置页面中的数据
+          longitude: res.data.shop.longitude,
+          latitude: res.data.shop.latitude,
+          tempProvince: res.data.shop.province,
+          tempCity: res.data.shop.city,
+          tempDistrict: res.data.shop.district,
+          markers: [{
+            id: 0,
             latitude: res.data.shop.latitude,
-            markers: [{
-              id: 0,
-              latitude: this.data.latitude,
-              longitude: this.data.longitude,
-              iconPath: '../../images/round-pushpin_1f4cd.png',//图标路径
-              width: 30,
-              height: 30,
-            }],
-          }
-        )
+            longitude: res.data.shop.longitude,
+            iconPath: '/images/round-pushpin_1f4cd.png', //图标路径
+            width: 30,
+            height: 30,
+          }, ],
+        })
+        if(shop.isMobile==0)
+        {
+          that.setData({
+            current: "固定商铺"
+          })
+        }else if(shop.isMobile==1){
+          that.setData({
+            current: "移动商铺"
+          })
+        }
+        this.moveToLocation;
+        console.log(this.data.markers);
       }
     })
-
-    this.mapCtx = wx.createMapContext('myMap');
-    this.mapCtx.moveToLocation();
 
     //实体化qqmap(API)核心类
     qqmapsdk = new QQMapWX({
@@ -175,52 +186,182 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
+  moveToPosition: function() {
+    var that = this;
+    wx.getLocation({
+      type: "gcj02",
+      success: function(res) {
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude
+        });
+      },
+    })
+    this.mapCtx.moveToLocation();
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-    
+  buttonClickMoveMap: function(e) {
+    var that = this;
+    var _address = this.data.tempProvince + this.data.tempCity + this.data.tempDistrict + this.data.shop.fullAddress;
+    var _province = "shop.province";
+    var _city = "shop.city";
+    var _district = "shop.district";
+    console.log(_address);
+    qqmapsdk.geocoder({
+      address: _address,
+      success: function(res) {
+        //成功后的回调
+        console.log(res);
+        var res = res.result;
+        var _latitude = res.location.lat;
+        var _longitude = res.location.lng;
+        that.setData({
+          longitude: _longitude,
+          latitude: _latitude,
+          [_province]: res.address_components.province,
+          [_city]: res.address_components.city,
+          [_district]: res.address_components.district,
+          markers: [{
+            id: 0,
+            latitude: _latitude,
+            longitude: _longitude,
+            iconPath: '/images/round-pushpin_1f4cd.png', //图标路径
+            width: 30,
+            height: 30,
+          }],
+        })
+        this.moveToPosition;
+      },
+      fail: function(error) {
+        console.error(error);
+      },
+      complete: function(res) {
+        console.log(res);
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
+  //确认修改上传数据
+  buttonClickCommit: function(e) {
+    var that = this;
 
+    if (that.data.shop.shopName.length == 0) {
+      wx.showModal({
+        title: '提示',
+        content: '请填写您的商铺名称',
+      })
+    } else if (that.data.shop.phone.length == 0) {
+      wx.showModal({
+        title: '提示',
+        content: '请填写商铺的联系电话',
+      })
+    } else if (that.data.shop.businessScope.length == 0) {
+      wx.showModal({
+        title: '提示',
+        content: '请填写商铺的经营范围',
+      })
+    } else if (that.data.shop.perCost.length == 0) {
+      wx.showModal({
+        title: '提示',
+        content: '请填写商铺的人均消费',
+      })
+    } else if (that.data.shop.businessLicenseCode.length == 0) {
+      wx.showModal({
+        title: '提示',
+        content: '请填写商铺的执照编码',
+      })
+    } else if (that.data.shop.fullAddress.length == 0) {
+      wx.showModal({
+        title: '提示',
+        content: '请您输入商店的完整地址',
+      })
+    } else {
+      var token = null;
+      try {
+        const value = wx.getStorageSync('token')
+        if (value) {
+          token = value;
+          console.log(token);
+        }
+      } catch (e) {
+        console.log("error");
+      }
+      console.log(that.data.shop);
+      wx.request({
+        url: app.globalData.serviceUrl + "/SearchStreet/shopadmin/modifyshop?token=" + token,
+        data: {
+          shopId: this.data.shop.shopId,
+          shopName: this.data.shop.shopName,
+          businessScope: this.data.shop.businessScope,
+          perCost: this.data.shop.perCost,
+          businessLicenseCode: this.data.shop.businessLicenseCode,
+          phone: this.data.shop.phone,
+          province: this.data.shop.province,
+          city: this.data.shop.city,
+          district: this.data.shop.district,
+          fullAddress: this.data.shop.fullAddress,
+          longitude: this.data.longitude,
+          latitude: this.data.latitude,
+          shopMoreInfo: this.data.shop.shopMoreInfo,
+          isMobile: this.data.shop.isMobile
+        },
+        method: "POST",
+        success: res => {
+          console.log(res);
+          wx.navigateBack({
+            
+          });
+        }
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
+/**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+onReady: function() {
 
-  },
+},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
+/**
+ * 生命周期函数--监听页面显示
+ */
+onShow: function() {
 
-  },
+},
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
+/**
+ * 生命周期函数--监听页面隐藏
+ */
+onHide: function() {
 
-  },
+},
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
+/**
+ * 生命周期函数--监听页面卸载
+ */
+onUnload: function() {
 
-  }
+},
+
+/**
+ * 页面相关事件处理函数--监听用户下拉动作
+ */
+onPullDownRefresh: function() {
+
+},
+
+/**
+ * 页面上拉触底事件的处理函数
+ */
+onReachBottom: function() {
+
+},
+
+/**
+ * 用户点击右上角分享
+ */
+onShareAppMessage: function() {
+
+}
 })
